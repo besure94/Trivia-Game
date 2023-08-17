bfd1bb45f2225c244ad7507cfa980673ff77323b
Brian Scherner <brianscherner@gmail.com>
2023-08-17 14:34:44 -0700

    Update root directory name in files.

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
 			verbose: true
 		}),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Trivia Flash Cards',
       template: './src/index.html',
       inject: 'body'
     }),
