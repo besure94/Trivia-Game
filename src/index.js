e0ddc4600d7f11d60551a297c1ee58bb9714015e
Brian Scherner <brianscherner@gmail.com>
2023-08-17 18:51:23 -0700

    Update API functionality with functions and event listeners.

--- src/index.js
+++ src/index.js
@@ -10,12 +10,43 @@ function getTrivia(searchType) {
   } else if (searchType === "film") {
     url = `https://opentdb.com/api.php?amount=10&category=11&difficulty=easy`;
   } else if (searchType === "tv") {
-    url = `https://opentdb.com/api.php?amount=10&category=14`;
+    url = `https://opentdb.com/api.php?amount=10&category=14&difficulty=easy`;
   } else if (searchType === "geography") {
-    url = `https://opentdb.com/api.php?amount=10&category=22`;
+    url = `https://opentdb.com/api.php?amount=10&category=22&difficulty=easy`;
   } else if (searchType === "history") {
-    url = `https://opentdb.com/api.php?amount=10&category=23`;
+    url = `https://opentdb.com/api.php?amount=10&category=23&difficulty=easy`;
   } else if (searchType === "politics") {
-    url = `https://opentdb.com/api.php?amount=10&category=24`;
+    url = `https://opentdb.com/api.php?amount=10&category=24&difficulty=easy`;
   }
+
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements(response);
+    } else {
+      printError(response);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
 }
+
+function printElements(response) {
+  console.log(response);
+}
+
+function printError(response) {
+  console.log(response);
+  document.querySelector("#showTrivia").innerHTML = `There was an error accessing trivia:`
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const radioButtons = document.querySelector("input[name='trivia']:checked").value;
+  getTrivia(radioButtons);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
