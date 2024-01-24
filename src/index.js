245bdac8759ef695d3a227eb16c9f964ae359a03
Brian Scherner <brianscherner@gmail.com>
2024-01-24 13:05:05 -0800

    Adjust card width and change name of function that prints trivia question.

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import OpenTriviaApi from './js/openTriviaApi';
 async function getTriviaQuestions() {
   let response = await OpenTriviaApi.getTriviaQuestions();
   if (response[0].response_code == 0) {
-    populateTriviaCards(response);
+    populateTriviaCard(response);
   }
 }
 
-function populateTriviaCards(response) {
+function populateTriviaCard(response) {
   const triviaQuestionKeys = response[0].results;
   const randomTriviaQuestion = triviaQuestionKeys[Math.floor(Math.random() * triviaQuestionKeys.length)];
   let triviaCardDiv = document.getElementById("triviaCards");
